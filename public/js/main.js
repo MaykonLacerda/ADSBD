715788831a5bf0fff6add244f4f804f32d79a23a
Maykon Lacerda <maykonalvslacerda@gmail.com>
2020-04-22 19:15:16 -0300

    fix: integrate angular-recaptcha

--- public/js/main.js
+++ public/js/main.js
@@ -4,8 +4,10 @@ angular.module('angularApp', [
   'angular-loading-bar',
   'ngAnimate',
   'ngMaterial',
-  'ngMessages'
-])
+  'ngMessages',
+  'vcRecaptcha'
+])  
+
   .config(($routeProvider, $httpProvider, $mdDateLocaleProvider) => {
     $mdDateLocaleProvider.formatDate = function (date) {
       var m = moment(date)
@@ -19,3 +21,38 @@ angular.module('angularApp', [
     $routeProvider.otherwise({ redirectTo: '/home' })
 
   })
+
+  .controller('recapCtrl',['vcRecaptchaService','$http',function(vcRecaptchaService,$http){
+    var vm = this;
+    vm.publicKey = "6LcwcewUAAAAAGkpnnrK9Bg5X0mhGJi5rfyR0lUa";
+
+    vm.sendMessage = function(){
+
+     /* vcRecaptchaService.getResponse() gives you the g-captcha-response */
+
+        if(vcRecaptchaService.getResponse() === ""){ //if string is empty
+            alert("Por favor, resolva o captcha!")
+        }else {
+            var post_data = {  //prepare payload for request
+                'name':vm.name,
+                'email':vm.email,
+                'phone':vm.phone,
+                'email':vm.email,
+                'g-recaptcha-response':vcRecaptchaService.getResponse()  //send g-captcah-response to our server
+            }
+
+
+/* MAKE AJAX REQUEST to our server with g-captcha-string */
+                $http.post('http://sitename.com/api/signup',post_data).success(function(response){
+                if(response.error === 0){
+                    alert("Successfully verified and signed up the user");
+                }else{
+                    alert("User verification failed");
+                }
+            })
+            .error(function(error){
+
+            })
+        }
+    }
+}])
